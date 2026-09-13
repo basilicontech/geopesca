614d67413ded2758252c120697ef285e99781fd5
Alfonso <alfonsoisman@gmail.com>
2026-09-13 18:51:26 +0200

    Sobrescribimos variables de Astra con la paleta GeoPesca 3

--- blog/geopesca-child/functions.php
+++ blog/geopesca-child/functions.php
@@ -3,7 +3,7 @@
  * GeoPesca Child Theme functions and definitions
  */
 
-add_action( 'wp_enqueue_scripts', 'geopesca_child_enqueue_styles', 999 );
+add_action( 'wp_enqueue_scripts', 'geopesca_child_enqueue_styles', PHP_INT_MAX );
 
 function geopesca_child_enqueue_styles() {
     wp_enqueue_style(
