824c7b640d50c1f84e534fff60c3c46f42296bf6
Alfonso <alfonsoisman@gmail.com>
2026-09-16 17:46:07 +0200

    arreglamos boton responsive 4

--- frontend/map.js
+++ frontend/map.js
@@ -2,11 +2,11 @@
 const map = L.map("map", {
   minZoom: 5,
   maxBounds: [
-    [27.0, -18.5], // Suroeste (El Hierro)
-    [44.0, 4.5], // Noreste (Galicia y Menorca)
+    [27.5, -18.5], // Suroeste (El Hierro)
+    [44.5, 4.5], // Noreste (Galicia y Menorca)
   ],
   maxBoundsViscosity: 1.0,
-}).setView([35.5, -7.0], 5);
+}).setView([40.0, -7.0], 5);
 
 // Capa base
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
